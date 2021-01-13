1bf672ece840d5b7fde2f121f29d8cb6b54b0c67
lstking <lstking@163.com>
2021-01-14 00:04:59 +0800

    Add files via upload

---
+++ VRClient/Assets/SteamVR/Scripts/SteamVR_StatusText.cs
@@ -0,0 +1,36 @@
+﻿//========= Copyright 2014, Valve Corporation, All rights reserved. ===========
+//
+// Purpose: Used to display SteamVR status messages
+//
+//=============================================================================
+
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class SteamVR_StatusText : SteamVR_Status
+{
+	Text text;
+
+	void Awake()
+	{
+		text = GetComponent<Text>();
+
+		if (mode == Mode.WhileTrue || mode == Mode.WhileFalse)
+			timer = fade * text.color.a;
+	}
+
+	protected override void SetAlpha(float a)
+	{
+		if (a > 0.0f)
+		{
+			text.enabled = true;
+			text.color = new Color(text.color.r, text.color.g, text.color.b, a);
+		}
+		else
+		{
+			text.enabled = false;
+		}
+	}
+}
+
